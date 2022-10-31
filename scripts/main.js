19d905c284f4e19b86874006e1897b7d5497ce48
Ken Lee <kdblitz@gmail.com>
2022-10-31 09:41:44 +0800

    Some code cleanup.

--- scripts/main.js
+++ scripts/main.js
@@ -2,12 +2,8 @@ import Modal from './Modal.js';
 
 const mainRef = document.querySelector('main');
 
-// const modal = new Modal('Click me!');
-
-// mainRef.append(modal.buttonTrigger());
-
 const myModal = new Modal(
-  'click me!',
+  'Click me',
   'Are you sure you want to continue?',
   () => {
     const p = document.createElement('p');
@@ -23,19 +19,19 @@ const myModal = new Modal(
 
 mainRef.append(myModal.modalTrigger());
 
-const mod = new Modal(
-  'another!',
-  'Second message',
-  () => {
-    const p = document.createElement('p');
-    p.append('You just clicked "Yes"');
-    mainRef.append(p);
-  },
-  () => {
-    const p = document.createElement('p');
-    p.append('You just clicked "Cancel"');
-    mainRef.append(p);
-  }
-);
+// const mod = new Modal(
+//   'another!',
+//   'Second message',
+//   () => {
+//     const p = document.createElement('p');
+//     p.append('You just clicked "Yes"');
+//     mainRef.append(p);
+//   },
+//   () => {
+//     const p = document.createElement('p');
+//     p.append('You just clicked "Cancel"');
+//     mainRef.append(p);
+//   }
+// );
 
-mainRef.append(mod.modalTrigger());
+// mainRef.append(mod.modalTrigger());
